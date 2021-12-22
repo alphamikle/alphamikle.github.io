49361288e0694ee3140a85f7ebc5d3feace2c0a0
alphamikle <xelioys@gmail.com>
2021-12-22 00:06:46 -0300

    - update high_low

--- high_low/flutter_service_worker.js
+++ high_low/flutter_service_worker.js
@@ -4,6 +4,10 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+"assets/assets/images/notFound_1.png": "ee5aaf0a800af12ce2442df66543c922",
+"assets/assets/images/notFound_2.png": "0fd5cd91c57f6dcd2ab00113f52588fa",
+"assets/assets/images/notFound_3.png": "52ddbb3e2d5108c148f16def124637b5",
+"assets/assets/images/notFound_4.png": "edcd378ce69b5851f772fb3a639154c7",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
 "assets/NOTICES": "55e441d42ae58b40981aaf54018be2fe",
@@ -17,9 +21,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "f274cb48e0140ec9baff9efa3e6ce54f",
-"/": "f274cb48e0140ec9baff9efa3e6ce54f",
-"main.dart.js": "f75c7f2862d478dcd678bbf4ba2a39e2",
+"index.html": "5628098c1b9c74199cef24fcdf73432e",
+"/": "5628098c1b9c74199cef24fcdf73432e",
+"main.dart.js": "b2316dae6e6999b3dea85254048a6cef",
 "manifest.json": "c9c0c5976e09e540e60c6f2fd514fefb",
 "version.json": "68449fb351ea1709fe0cba306dfcd32f"
 };
